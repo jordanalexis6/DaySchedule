8709a2cf7f6fb0f6011721660d145e1a6cd4920d
Jordan <jordan.a.neill@gmail.com>
2020-05-14 23:15:27 -0500

    display current day

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+  // set colors for past, present and future
+
+  // click into text block and saves into local storage when hit saze button
+});
