002733a4e54942916a94209a77d36e76403ff3e6
Jordan <jordan.a.neill@gmail.com>
2020-05-17 08:06:49 -0500

    pseudo and fixed ids

--- assets/main.js
+++ assets/main.js
@@ -1,24 +1,24 @@
-//logic to render local storage
-$("#9 .description").val(localStorage.getItem("9"));
-$("#10 .description").val(localStorage.getItem("10"));
-$("#11 .description").val(localStorage.getItem("11"));
-$("#12 .description").val(localStorage.getItem("12"));
-$("#1 .description").val(localStorage.getItem("1"));
-$("#2 .description").val(localStorage.getItem("2"));
-$("#3 .description").val(localStorage.getItem("3"));
-$("#4 .description").val(localStorage.getItem("4"));
-$("#5 .description").val(localStorage.getItem("5"));
-
-// on click events are at the bottom
-$(".saveBtn").click(function () {
-  let textValue = $(this).siblings(".description").val();
-  let timeKey = $(this).parent().attr("id");
-  localStorage.setItem(timeKey, textValue);
-});
-
 $(document).ready(function () {
+  // displaying current day on html
   $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
-  // set colors for past, present and future
-  // $("#description").addClass("past");
-  // click into text block and saves into local storage when hit saze button
+  // lets the js know what the current hour is to implement colors
+  // setting time to keep going up so we can use this in a for loop
+  // if the currentHour is less than the time displayed use correct classes
+  // if the currentHour is less than the time displayed use correct classes
+  // save button function
+  $(".saveBtn").click(function () {
+    let textValue = $(this).siblings(".description").val();
+    let timeKey = $(this).parent().attr("id");
+    localStorage.setItem(timeKey, textValue);
+  });
+  //render local storage
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
 });
